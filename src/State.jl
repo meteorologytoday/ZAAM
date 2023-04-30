3c5ac906c2a94157e6dba71b24c592b2e43936c7
t2hsu <t2hsu@aha.ucsd.edu>
2023-04-30 13:08:08 -0700

    Import matrix operators. Need to rearragne dimension order

--- src/State.jl
+++ src/State.jl
@@ -3,6 +3,7 @@ mutable struct State
     B   :: AbstractArray{Float64, 1}
     Γ   :: AbstractArray{Float64, 1}
     SST :: AbstractArray{Float64, 1}
+    Ψ   :: AbstractArray{Float64, 1}
 
     function State(
         ev :: Env,
@@ -12,11 +13,13 @@ mutable struct State
         B   = zeros( Float64, Ny ) 
         Γ   = zeros( Float64, Ny ) 
         SST = zeros( Float64, Ny ) 
+        Ψ   = zeros( Float64, Ny+1) 
 
         return new(
             B,
             Γ,
             SST,
+            Ψ,
         )    
     end
 end
